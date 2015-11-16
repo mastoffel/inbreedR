968822ed69245c2d47fc57065af171367b3ca69a
Thinkmarti <martin.adam.stoffel@gmail.com>
2015-11-16 11:07:51 +0100

    bug fixes simulate_g2

--- tests/testthat/test-simulate_g2.R
+++ tests/testthat/test-simulate_g2.R
@@ -5,11 +5,11 @@ data(mouse_msats)
 msats <- convert_raw(mouse_msats)
 
 test_that("g2 results matrix is computed", {
-    expect_equal(is.matrix(simulate_g2(n_ind = 20, subsets = c(2,4), reps = 10, mean_loc_MLH = 0.5, 
-                             sd_loc_MLH = 0.025)$estMat), TRUE)
+    expect_equal(is.matrix(simulate_g2(n_ind = 20, subsets = c(2,4), reps = 10, mean_MLH = 0.5, 
+                             sd_MLH = 0.025)$estMat), TRUE)
     expect_equal(is.matrix(simulate_g2(n_ind = 20, subsets = c(2,4), reps = 10, 
-                                       genotypes = msats, mean_loc_MLH = 0.5, 
-                                       sd_loc_MLH = 0.025)$estMat), TRUE)
+                                       genotypes = msats, mean_MLH = 0.5, 
+                                       sd_MLH = 0.025)$estMat), TRUE)
     expect_equal(is.matrix(simulate_g2(n_ind = 20, subsets = c(2,4), reps = 10, 
                                        genotypes = msats)$estMat), TRUE)
 })
